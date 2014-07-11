5e36c42177941156d2dec560b4fd96c19d279e56
Anatoly <theonlyfu@mail.ru>
2014-07-11 23:41:19 +0400

    elite

---
+++ hw-1/Sum.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Sum
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            foreach (string arg in args)
+            {
+                if (arg==string.Empty) continue;
+                string[] numbers = arg.Split(' ');
+                foreach (string number in numbers)
+                {
+                    sum += int.Parse(number);
+                }
+            }
+            Console.WriteLine("Результат - "+sum);
+        }
+    }
