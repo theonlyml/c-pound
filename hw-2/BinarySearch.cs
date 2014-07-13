e4866f4765d63953606e2f3b8d9d52a25d7414c0
Anatoly <theonlyfu@mail.ru>
2014-07-13 14:12:18 +0400

    so static

--- hw-2/BinarySearch.cs
+++ hw-2/BinarySearch.cs
@@ -1,55 +1,57 @@
 ﻿using System;
 
-    class BinarySearch
+internal class BinarySearch
+{
+    private static int middle;
+
+    public static int IterSearch(int x, int[] array)
     {
-        int left, right, mid;
-        public int IterSearch(int x,int[] array)
+        int left, right;
+        if (array.Length == 0)
         {
-            if (array.Length==0) 
-            {
-                Console.WriteLine("Введите массив");
-                return -1;
-            }
-            left = 0;
-            right = array.Length;
-            while (left<right)
-            {
-                mid = left + (right - left)/2;
-                if (x == array[mid]) return mid;
-                if (x > array[mid]) left = mid+1;
-                else right = mid;
-            }
-            if (array[mid] > x) return mid;
-            if (array[mid+1] > x) return mid+1;
+            Console.WriteLine("Введите массив");
             return -1;
         }
-
-        public int RecSearch(int x,int[] array,int left,int right)
+        left = 0;
+        right = array.Length;
+        while (left < right)
         {
-            if (array.Length == 0)
-            {
-                Console.WriteLine("Введите массив");
-                return -1;
-            }
-            if (left >= right)
-                if (array[mid] > x) return mid;
-                else if (array[mid+1] > x) return mid+1;
-                else return -1;
-            mid = left + (right - left) / 2;
-            if (x == array[mid]) return mid;
-            if (x > array[mid]) return RecSearch(x, array, mid+1, right);
-            return RecSearch(x, array, left, mid);
+            middle = left + (right - left)/2;
+            if (x == array[middle]) return middle;
+            if (x > array[middle]) left = middle + 1;
+            else right = middle;
         }
-
-        static void Main(string[] args)
-        {
-            int[] numbers=new int[args.Length-1];
-            int key = int.Parse(args[0]);
-            for (int i = 1; i < args.Length; i++)
-            {
-                numbers[i - 1] = int.Parse(args[i]);
-            }
-            Console.WriteLine(new BinarySearch().IterSearch(key,numbers));
-            Console.WriteLine(new BinarySearch().RecSearch(key,numbers,0,numbers.Length));
-        }
+        if (array[middle] > x) return middle;
+        if (array[middle + 1] > x) return middle + 1;
+        return -1;
     }
+
+    public static int RecSearch(int x, int[] array, int left, int right)
+    {
+        if (array.Length == 0)
+        {
+            Console.WriteLine("Введите массив");
+            return -1;
+        }
+        if (left >= right)
+            if (array[middle] > x) return middle;
+            else if (array[middle + 1] > x) return middle + 1;
+            else return -1;
+        middle = left + (right - left)/2;
+        if (x == array[middle]) return middle;
+        if (x > array[middle]) return RecSearch(x, array, middle + 1, right);
+        return RecSearch(x, array, left, middle);
+    }
+
+    private static void Main(string[] args)
+    {
+        var numbers = new int[args.Length - 1];
+        int key = int.Parse(args[0]);
+        for (int i = 1; i < args.Length; i++)
+        {
+            numbers[i - 1] = int.Parse(args[i]);
+        }
+        Console.WriteLine(IterSearch(key, numbers));
+        Console.WriteLine(RecSearch(key, numbers, 0, numbers.Length));
+    }
+}
